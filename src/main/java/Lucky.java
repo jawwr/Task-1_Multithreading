236f17385c022dea5b9a645089723db3da95dcb0
Arseny <20mineslend14@gmail.com>
2022-11-20 23:03:13 +0500

    sync method

--- src/main/java/Lucky.java
+++ src/main/java/Lucky.java
@@ -9,10 +9,12 @@ public class Lucky {
         public void run() {
             while (luckyX.get() < 999999) {
                 final int x = luckyX.incrementAndGet();
-                if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
-                        % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
-                    System.out.println(Thread.currentThread().getName() + ": " + x);
-                    count.incrementAndGet();
+                synchronized (Object.class) {
+                    if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000) % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
+                        System.out.println(Thread.currentThread().getName() + ": " + x);
+
+                        count.incrementAndGet();
+                    }
                 }
             }
         }
